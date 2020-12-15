881f6d1fc3dc9bbae7594c38fb752288e422a311
Thirunavukkarasu <thirunavukkarasu.mscse@gmail.com>
2020-12-15 16:14:20 +0000

    Completed GetQuestions functionality

--- Survey.cs
+++ Survey.cs
@@ -9,44 +9,68 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Data.SqlClient;
 using System.Data;
+using System.Collections.Generic;
 
-namespace Cognizant.Survey
+namespace Cognizant.Question
 {
-    public static class Survey
+    public static class GetQuestion
     {
-        [FunctionName("Survey")]
+        [FunctionName("GetQuestion")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string recordId = null;
+            var strConnString = Environment.GetEnvironmentVariable("SqlServerConnection");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            recordId = recordId ?? data?.recordId;
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //recordId = recordId ?? data?.recordId;
             string responseMessage = string.Empty;
-            if (!String.IsNullOrEmpty(recordId))
+            using (SqlConnection connection = new SqlConnection(strConnString))
             {
-                var strConnString = Environment.GetEnvironmentVariable("SqlServerConnection");
-                using (SqlConnection con = new SqlConnection(strConnString))
+                SqlCommand command = new SqlCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "GetQuestions";
+                command.Connection = connection;
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                IList<Questions> questions = new List<Questions>();
+
+                // Call Read before accessing data.
+                while (reader.Read())
                 {
-                    SqlCommand cmd = new SqlCommand();
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.CommandText = "InsertSurvey";
-                    cmd.Parameters.Add("@Record_id", SqlDbType.Int).Value = Convert.ToInt32(recordId);
-
-                    cmd.Connection = con;
-
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    log.LogInformation("Survey updated record");
+                    var question = ReadSingleRow((IDataRecord)reader, log);
+                    questions.Add(question);
                 }
-                responseMessage =  "Medical record successfully";
+                responseMessage = JsonConvert.SerializeObject(questions);
+                log.LogInformation(responseMessage);
+                // Call Close when done reading.
+                reader.Close();
             }
             return new OkObjectResult(responseMessage);
             
         }
+
+        private static Questions ReadSingleRow(IDataRecord record, ILogger log)
+        {
+            Questions question = new Questions()
+            {
+                Question = Convert.ToString(record[0]),
+                ID = Convert.ToInt32(record[1]),
+                Type = Convert.ToString(record[2])
+            };
+
+            //log.LogInformation(String.Format("{0}, {1}, {2)", record[0], record[1], record[2]));
+            return question;
+        }
+    }
+    public class Questions
+    {
+        public string Question { get; set; }
+        public int ID { get; set; }
+        public string Type { get; set; }
     }
 }
